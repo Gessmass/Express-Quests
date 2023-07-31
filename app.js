fd6c691edf8192f9dd03f86b8d6f5e67005172f9
Gessmass <martinweilldrums@gmail.com>
2023-07-31 16:17:13 +0200

    je delete

--- app.js
+++ app.js
@@ -39,3 +39,5 @@ app.post("/api/users", validateUser, userHandlers.postUser);
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
